21bf46b2fe0b5a8a500e8ded9648432def76795b
Bread White <breadrock1@email.net>
2024-11-11 17:15:46 +0300

    chore(migrations): added additional table fields

--- migrations/20241111105852_add-rss-sources.up.sql
+++ migrations/20241111105852_add-rss-sources.up.sql
@@ -4,7 +4,10 @@ CREATE TABLE IF NOT EXISTS rss_sources(
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL,
     link TEXT NOT NULL,
-    run_at_launch BOOL NOT NULL DEFAULT false
+    run_at_launch BOOL NOT NULL DEFAULT false,
+    max_retries INTEGER NOT NULL DEFAULT 3,
+    timeout INTEGER NOT NULL DEFAULT 100,
+    interval_secs INTEGER NOT NULL DEFAULT 3600
 );
 
 INSERT INTO rss_sources(name, link, run_at_launch)
